ac11db6e799795242df9cbd8cacdc53987dca652
jaitmal singh <jaitmal7773@gmail.com>
2025-09-14 02:23:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "24473318"
-api_hash = "e7dd0576c5ac0ff8f90971d6bb04c8f5"
-bot_token = "8034069514:AAHUBpzSCq41jPwsJkDbXuEoVC_yCxzyuw0"
-auth_users = [5840594311]
+api_id = "20977199"
+api_hash = "de8cf36cc1d274a70206b990259a830e"
+bot_token = "8244226484:AAGQ_LagDLOyENECCtlwgSHLFKearTawP3A"
+auth_users = [6231180860]
