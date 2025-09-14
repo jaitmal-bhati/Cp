2c6f89a7df4bfdfa2574bda3302bb9f334fd66b3
jaitmal singh <jaitmal7773@gmail.com>
2025-09-14 12:56:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "20977199"
-api_hash = "de8cf36cc1d274a70206b990259a830e"
+api_id = "25075525"
+api_hash = "496983c6d591fd6b5999a9ddf9e99c92"
 bot_token = "8244226484:AAGQ_LagDLOyENECCtlwgSHLFKearTawP3A"
-auth_users = [6231180860]
+auth_users = [5722206227]
